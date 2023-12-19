eb3c383ea97ade0ef71e4a571d5b2002def0a878
Karan Joshi <Karan190213@gmail.com>
2023-12-19 14:08:56 +0530

    Implement methods archived

--- classes/item.rb
+++ classes/item.rb
@@ -12,4 +12,29 @@ class Item
     @publish_date = publish_date
     @archived = false
   end
+
+def genre=(new_genre)
+  @genre = new_genre
+end
+
+def author=(new_author)
+  @author = new_author
+end
+
+def label=(new_label)
+  @label = new_label
+end
+
+def can_be_archived?
+  (Time.now.year - @publish_date.year) > 10
+end
+
+def move_to_archive
+  if can_be_archived
+    @archived = true
+    puts 'Item #{id} has been archived'
+  else
+    puts 'Item #{id} cannot be archived'
+  end
+end
 end
