1700c0b5466ea6794d77a677b1600046b0074ac7
Karan Joshi <Karan190213@gmail.com>
2023-12-19 15:57:17 +0530

    Linters error fixed

--- classes/item.rb
+++ classes/item.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
-# class Item.rb
 class Item
-  attr_accessor :id, :genre, :author, :label, :publish_date, :archived
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(genre, author, label, publish_date)
+  def initialize(genre, author, source, label, publish_date)
     @id = Random.rand(1..1000)
     @genre = genre || {}
     @author = author || {}
@@ -14,32 +11,16 @@ class Item
     @archived = false
   end
 
-def genre=(new_genre)
-  @genre = new_genre
-end
-
-def author=(new_author)
-  @author = new_author
-end
-
-def source=(new_source)
-  @source = new_source
+  def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
   end
 
-def label=(new_label)
-  @label = new_label
-end
-
-def can_be_archived?
-  (Time.now.year - @publish_date.year) > 10
-end
-
-def move_to_archive
-  if can_be_archived
-    @archived = true
-    puts 'Item #{id} has been archived'
-  else
-    puts 'Item #{id} cannot be archived'
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts "Item #{@id} has been archived"
+    else
+      puts "Item #{@id} cannot be archived"
+    end
   end
-end
 end
