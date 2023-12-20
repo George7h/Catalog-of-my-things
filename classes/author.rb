0300603886ca5cab1f0be3424bde02e08c5c78f1
Karan Joshi <Karan190213@gmail.com>
2023-12-20 18:16:32 +0530

    author.rb updated

--- classes/author.rb
+++ classes/author.rb
@@ -4,7 +4,7 @@ require_relative 'item'
 class Author
   attr_accessor :id, :first_name, :last_name, :items
 
-  def initialize(id:, first_name:, last_name:)
+  def initialize(id: nil, first_name:, last_name:)
     @id = id
     @first_name = first_name
     @last_name = last_name
@@ -21,14 +21,10 @@ class Author
   end
 
   def self.all
-    # Return all instances of Author (you may need to maintain a collection)
-    # Replace this with your actual data retrieval logic
     []
   end
 
   def self.find_by_full_name(full_name)
-    # Find and return an author by full name
-    # Replace this with your actual data retrieval logic
     nil
   end
 end
