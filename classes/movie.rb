31329acc6a04c80176fc59e9e1b314e961c88174
Obimbo07 <obimboausts@gmail.com>
2023-12-20 10:29:16 +0300

    linters-fix

--- classes/movie.rb
+++ classes/movie.rb
@@ -1,13 +1,14 @@
 require './classes/item'
 
 class Movie < item
- attr_accessor :silent
- def initialize(genre, author, source, label, publish_date, silent)
-    super(genre, author, source, label, publish_date)
-    @silent = silent
- end
+  attr_accessor :silent
 
- def can_be_archived?
+  def initialize(genre, author, source, publish_date, silent)
+    super(genre, author, source, publish_date)
+    @silent = silent
+  end
+
+  def can_be_archived?
     super || @silent
- end
+  end
 end
--- classes/source.rb
+++ classes/source.rb
@@ -1,16 +1,18 @@
 require './classes/item'
 
 class Source < Item
-    attr_accessor :id, :name, :items
+  attr_accessor :name,
+  attr_reader :id, :items
 
-    def initialize(id, name, items)
-        @id = id
-        @name = name
-        @items = []
-    end
+  def initialize(name, id = nil)
+    super
+    @id = id || Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.label = self
-    end
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
