8b9d7af7bf559b5677eda7210c0447ff0d69e442
Fombi-Favour <fombifavour@gmail.com>
2023-12-20 14:00:30 +0100

    add genre.rb

---
+++ classes/genre.rb
@@ -0,0 +1,18 @@
+require './classes/item'
+
+class Genre < Item
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialize(name, id = nil)
+    super
+    @id = id || Random.random(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
