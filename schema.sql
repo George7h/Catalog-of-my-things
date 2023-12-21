cc2f478e05be06bd23faa04624559949b4a02b6a
Karan Joshi <Karan190213@gmail.com>
2023-12-21 13:37:29 +0530

    update schema

--- schema.sql
+++ schema.sql
@@ -9,11 +9,11 @@ CREATE TABLE authors (
 CREATE TABLE games (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   genre TEXT,
+  author_id INTEGER,
   source TEXT,
   label TEXT,
   publish_date DATE,
   multiplayer BOOLEAN,
   last_played_at DATE,
-  author_id INTEGER,
   FOREIGN KEY (author_id) REFERENCES authors(id)
 );
