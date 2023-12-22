c66582fbf8debe23dad07e36b1113301bff4e2fb
George <georgie.george7@gmail.com>
2023-12-22 11:58:08 +0400

    Refactor Book class to include to_json method

--- classes/book.rb
+++ classes/book.rb
@@ -4,16 +4,30 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date)
-    super(nil, nil, nil, nil, publish_date)
+    super(nil, nil, nil, cover_state, publish_date)
     @publisher = publisher
     @cover_state = cover_state
+    @archived = can_be_archived?
   end
-
-  private
 
   def can_be_archived?
     return true if @cover_state.downcase == 'bad'
 
     false
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'genre' => @genre,
+      'author' => @author,
+      'source' => @source,
+      'label' => @label,
+      'publish_date' => @publish_date,
+      'archived' => @archived,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state
+    }.to_json(*args)
   end
 end
