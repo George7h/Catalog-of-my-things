1e9c5cd1a49675ebfd01e5866d93366baf51f6df
George <georgie.george7@gmail.com>
2023-12-22 16:17:43 +0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,36 @@
+CREATE TABLE Items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  published_date DATE,
+  archived BOOLEAN,
+)
+
+-- Labels Table
+CREATE TABLE Labels (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  color VARCHAR(255) NOT NULL,, 
+  published_date DATE,
+  item_id INT,
+  items TEXT[],
+  Foreign Key (item_id) REFERENCES (items_id)
+  PRIMARY KEY (id)
+);
+
+-- Books Table
+CREATE TABLE Books (
+    id INT GENERATED ALWAYS AS IDENTITY
+    item_id INT,
+    title VARCHAR(255),
+    author_id INT,
+    label_id Int,
+    cover_state VARCHAR(255),
+    publisher VARCHAR(255),
+    FOREIGN KEY (item_id) REFERENCES items(id),
+    FOREIGN KEY (label_id) REFERENCES labels(id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
+    PRIMARY KEY(id)
+);
+
 -- Authors Table
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
