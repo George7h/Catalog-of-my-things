ceadb50f19aae87fa77aa0dea4da32d5bc0654ca
Karan Joshi <Karan190213@gmail.com>
2023-12-22 14:09:36 +0530

    Update test cases

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,50 +1,30 @@
-# game_spec.rb
-require 'date'
+# spec/game_spec.rb
+
+require 'rspec'
 require_relative '../classes/game'
 
 describe Game do
-  let(:current_time) { Time.now }
-  let(:ten_years_ago) { current_time - (10 * 365 * 24 * 60 * 60) }
-  let(:two_years_ago) { current_time - (2 * 365 * 24 * 60 * 60) }
+  let(:publish_date) { '2010-01-01' }
+  let(:multiplayer) { true }
+  let(:last_played_at) { '2022-01-01' }
 
-  describe '#initialize' do
-    it 'initializes a Game object with the provided attributes' do
-      game = Game.new('FPS', 'John Doe', 'Steam', 'Call of Duty', '2020-01-01', true, '2023-01-01')
-      expect(game.genre).to eq('FPS')
-      expect(game.author).to eq('John Doe')
-      expect(game.source).to eq('Steam')
-      expect(game.label).to eq('Call of Duty')
-      expect(game.publish_date).to eq(Date.parse('2020-01-01'))
-      expect(game.multiplayer).to be true
-      expect(game.last_played_at).to eq('2023-01-01')
-    end
-  end
+  subject(:game) { described_class.new(publish_date, multiplayer, last_played_at) }
 
   describe '#can_be_archived?' do
     context 'when the game can be archived' do
       it 'returns true' do
-        game = Game.new('RPG', 'Jane Smith', 'Origin', 'Mass Effect', '2010-01-01', true, two_years_ago.strftime('%F'))
-        expect(game.can_be_archived?).to be true
+        allow(Time).to receive(:now).and_return(Time.new(2032, 1, 1, 0, 0, 0))
+
+        expect(game.can_be_archived?).to be(true)
       end
     end
 
     context 'when the game cannot be archived' do
       it 'returns false' do
-        game = Game.new('Adventure', 'Bob Johnson', 'Epic Games', 'Uncharted', '2022-01-01', false,
-                        two_years_ago.strftime('%F'))
-        expect(game.can_be_archived?).to be false
+        allow(Time).to receive(:now).and_return(Time.new(2015, 1, 1, 0, 0, 0))
+
+        expect(game.can_be_archived?).to be(false)
       end
-    end
-  end
-
-  describe '.all' do
-    it 'returns an array of all game instances' do
-      Game.class_variable_set(:@@all_games, []) # Reset the class variable before testing
-
-      game1 = Game.new('Strategy', 'Alice Wonderland', 'GOG', 'Civilization VI', '2018-01-01', true, '2023-01-01')
-      game2 = Game.new('Simulation', 'Eve Adams', 'Ubisoft', 'The Sims 4', '2015-01-01', false, '2022-01-01')
-
-      expect(Game.all).to contain_exactly(game1, game2)
     end
   end
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,31 +1,32 @@
+require 'date'
 require './classes/movie'
 
 RSpec.describe Movie do
   describe '#initialize' do
     it 'creates a new movie with the given attributes' do
-      movie = Movie.new('Sci-Fi', 'Christopher Nolan', 'Hollywood', Date.parse('2023-01-01'), true)
-      expect(movie.genre).to eq('Sci-Fi')
-      expect(movie.author).to eq('Christopher Nolan')
-      expect(movie.source).to eq('Hollywood')
-      expect(movie.publish_date).to eq(Date.parse('2023-01-01'))
-      expect(movie.silent).to eq(true)
-      expect(movie.id).not_to be_nil
+      publish_date = Date.parse('2023-01-01')
+      silent = true
+
+      movie = Movie.new(publish_date, silent)
+
+      expect(movie.publish_date).to eq(publish_date)
+      expect(movie.silent).to eq(silent)
     end
   end
 
   describe '#can_be_archived?' do
-    context 'when the movie is silent' do
+    context 'when silent is true' do
       it 'returns true' do
-        movie = Movie.new('Sci-Fi', 'Christopher Nolan', 'Hollywood', Date.parse('2023-01-01'), true)
-        expect(movie.can_be_archived?).to eq(true)
+        movie = Movie.new(Date.parse('2023-01-01'), true)
+        expect(movie.can_be_archived?).to be true
       end
     end
 
-    context 'when the movie is not silent' do
-      it 'returns the result of the parent method' do
-        movie = Movie.new('Sci-Fi', 'Christopher Nolan', 'Hollywood', Date.parse('2023-01-01'), true)
-        allow(movie).to receive(:super).and_return(true)
-        expect(movie.can_be_archived?).to eq(true)
+    context 'when silent is false' do
+      it 'returns the result from the superclass' do
+        allow_any_instance_of(Item).to receive(:can_be_archived?).and_return(false)
+        movie = Movie.new(Date.parse('2023-01-01'), false)
+        expect(movie.can_be_archived?).to be false
       end
     end
   end
