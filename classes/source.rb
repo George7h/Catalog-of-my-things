8e9c8cd4cf050f55cdadfd8ca905f7501bc2faeb
Obimbo07 <obimboausts@gmail.com>
2023-12-22 11:07:10 +0300

    source.rb

--- classes/source.rb
+++ classes/source.rb
@@ -1,6 +1,6 @@
 require './classes/item'
 
-class Source < Item
+class Source
   attr_accessor :name
   attr_reader :id, :items
 
@@ -9,6 +9,13 @@ class Source < Item
     @id = id || Random.rand(1..1000)
     @name = name
     @items = []
+  end
+
+  def to_json(*args)
+    {
+      'id' => @id,
+      'name' => @name
+    }.to_json(*args)
   end
 
   def add_item(item)
